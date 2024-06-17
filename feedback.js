725a53960c0c01dba1d67f12f6d728bbf0ca1ecd
najlaaalsmadi <najlaak399@gmail.com>
2024-06-18 02:49:00 +0300

    update

--- feedback.js
+++ feedback.js
@@ -1,8 +1,8 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("click", function() {
     let storedData = localStorage.getItem("contactMessages"); // Corrected the key name
     console.log(storedData);
     let feedbacks = [];
-  
+ 
     // Check if there is stored data
     if (storedData) {
         // Parse stored JSON string to array
@@ -53,3 +53,22 @@ document.addEventListener("DOMContentLoaded", function() {
     createFeedbackCards('feedback-cards-top', feedbacks.slice(0, 3)); // Display top 3 feedbacks
     createFeedbackCards('feedback-cards-bottom', feedbacks.slice(3, 6)); // Display next 3 feedbacks
   });
+
+
+  // Function to toggle dark mode
+function toggleDarkMode() {
+    let body = document.body;
+    body.classList.toggle('dark-mode');
+    if (body.classList.contains('dark-mode')) {
+        localStorage.setItem('darkMode', true);
+    } else {
+        localStorage.removeItem('darkMode');
+    }
+}
+
+window.onload = function() {
+    let isDarkMode = localStorage.getItem('darkMode');
+    if (isDarkMode === 'true') {
+        document.body.classList.add('dark-mode');
+    }
+};
